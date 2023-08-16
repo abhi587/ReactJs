b5ea14c4f2f82043660d67847c087bdc8c7d45c1
abhi587 <abhishek.pawan14@gmail.com>
2023-08-16 15:29:09 +0530

    props/components

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+const videos = [{
+    id: 16,
+    title: "React Js tutor",
+    views: "999M",
+    time: "56 month ago",
+    channel: "Code lover",
+    verified: true,
+  },
+  {
+    id:96,
+    title: "Node Js tutor",
+    views: "13M",
+    time: "5 Year ago",
+    channel: "Code lover",
+    verified: false,
+  },
+  {
+    id:118,
+    title: "MongoDB Js tutor",
+    views: "21B",
+    time: "10 Year ago",
+    channel: "Code lover",
+    verified: true,
+  }]
+
+  export default videos;
